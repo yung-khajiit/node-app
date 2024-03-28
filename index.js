922b2451a79b1969d946486274957e749e0542e0
yung-khajiit <jeenriquez1219@gmail.com>
2024-03-28 08:22:17 -0500

    Installed cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import route from "./routes/employeeRoutes.js"
+import cors from "cors"
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 dotenv.config()
 
